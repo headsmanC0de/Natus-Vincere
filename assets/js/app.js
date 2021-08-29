1bd944de106148173c3fd31945269a54fcc4b6f5
headsmanс0de <38921141+headsmanC0de@users.noreply.github.com>
2021-08-30 00:26:11 +0300

    Rebuilding js structure

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,50 +1,48 @@
 // Feedback Area
 const appels = document.getElementById("appeals");
-
 // Send Button
 const sendButton = document.querySelector(".btn-send");
 
-sendButton.addEventListener('click', function() {
-    const article = document.createElement("article");
-article.setAttribute("class", "feedback");
+let createAppeal = function (text) {
+  const article = document.createElement("article");
+  article.setAttribute("class", "feedback");
 
-appels.appendChild(article);
+  appels.appendChild(article);
 
-const textDiv = document.createElement("div");
-const feedbackText = document.createElement("p");
-textDiv.appendChild(feedbackText);
+  const textDiv = document.createElement("div");
+  const feedbackText = document.createElement("p");
+  textDiv.appendChild(feedbackText);
+  feedbackText.textContent = text;
 
-const dateAuthor = document.createElement("div");
-dateAuthor.setAttribute('class','date__author')
-article.appendChild(textDiv);
-article.appendChild(dateAuthor);
+  const dateAuthor = document.createElement("div");
+  dateAuthor.setAttribute("class", "date__author");
+  article.appendChild(textDiv);
+  article.appendChild(dateAuthor);
 
+  const date = document.createElement("div");
+  date.setAttribute("class", "date");
 
-const date = document.createElement("div");
-date.setAttribute('class', 'date')
+  // Time Setting
+  const newDate = new Date();
+  date.textContent = newDate.toLocaleString();
 
-// Time Setting
-const newDate = new Date();
-date.textContent = newDate.toLocaleString()
+  const author = document.createElement("div");
+  author.setAttribute("class", "author");
+  dateAuthor.appendChild(date);
+  dateAuthor.appendChild(author);
 
-const author = document.createElement("div");
-author.setAttribute('class', 'author')
-dateAuthor.appendChild(date)
-dateAuthor.appendChild(author)
+  const authorLink = document.createElement("a");
+  author.appendChild(authorLink);
+  authorLink.setAttribute("href", "https://headsmanc0de.com/");
+  authorLink.setAttribute("target", "_blank");
+  authorLink.textContent = "headsmanc0de";
+};
 
+function addAppeal() {
+  const textArea = document.getElementById("feedback-text");
+  const textAreaText = textArea.value;
+  createAppeal(textAreaText);
+  textArea.value = "";
+}
 
-const authorLink = document.createElement("a");
-author.appendChild(authorLink);
-authorLink.setAttribute("href", "https://headsmanc0de.com/");
-authorLink.setAttribute("target", "_blank");
-
-authorLink.textContent = "headsmanc0de";
-
-// Text area
-const textArea = document.getElementById("feedback-text");
-const textAreaText = textArea.value;
-
-feedbackText.textContent = textAreaText;
-
-})
-
+sendButton.addEventListener("click", addAppeal);
